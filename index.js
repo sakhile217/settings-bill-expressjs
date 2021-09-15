9571c5bd7b1a47d15d80df715729d55a0794db81
Sakhile217 <sakhilebaloyi1@gmail.com>
2021-09-15 13:32:08 +0200

    settings bill express

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require('./settings-bill');
 
 const app = express();
+const settingsBill = SettingsBill();
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
-    res.render('layouts');
+    res.render('layouts', {settings: settingsBill.getSettings});
 });
 
 app.listen(3011, function(){
@@ -17,7 +26,19 @@ app.listen(3011, function(){
 });
 
 app.post('/settings', function(req, res) {
+console.log(req.body);
 
+settingsBill.setSettings({
+callCost: req.body.callCost,
+smsCost: req.body.smsCost,
+warningLevel: req.body.warningLevel,
+criticalLevel: req.body.criticalLevel
+
+});
+
+console.log(settingsBill.getSettings());
+
+res.redirect('/');
 });
 
 app.post('/action', function(req, res){
@@ -33,3 +54,6 @@ app.get('/actions/:type', function(req, res){
 });
 
 const PORT = process.env.PORT || 3011;
+
+
+//value="{{settings.callCost}}" insert this on all input of settings
